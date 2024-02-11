0dfb1c92bc0e521055850a72d5ce64a183f36680
FatChapa <maksimka.sharov2008@gmail.com>
2024-02-11 16:41:11 +0300

    Fall Velocity zero is grounded

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -58,5 +58,10 @@ public class PlayerController : MonoBehaviour
 
         _fallVelocity += gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if (_characterController.isGrounded) 
+        {
+            _fallVelocity = 0;        
+        }
     }
 }
