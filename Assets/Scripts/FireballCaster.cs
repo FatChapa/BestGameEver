3f6645e798533c49b457b3ce48c1f5764e38b410
FatChapa <maksimka.sharov2008@gmail.com>
2024-03-10 00:32:27 +0300

    Add fireballSourceTransform (script)

--- Assets/Scripts/FireballCaster.cs
+++ Assets/Scripts/FireballCaster.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FireballCaster : MonoBehaviour
 {
     public Fireball fireballPrefab;
+    public Transform fireballSourceTransform;
     void Start()
     {
         
@@ -15,7 +16,7 @@ public class FireballCaster : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(fireballPrefab, transform.position, transform.rotation);
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
 
         }
     }
