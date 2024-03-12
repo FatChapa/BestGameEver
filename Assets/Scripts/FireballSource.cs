8ef139236df705708bdb2b0179a0e0e10c48c14b
FatChapa <maksimka.sharov2008@gmail.com>
2024-03-12 21:53:02 +0300

    Add targetInSkyDistance (script)

--- Assets/Scripts/FireballSource.cs
+++ Assets/Scripts/FireballSource.cs
@@ -6,6 +6,7 @@ public class FireballSource : MonoBehaviour
 {
     public Transform targetPoint;
     public Camera cameraLink;
+    public float targetInSkyDistance;
     void Start()
     {
         
@@ -14,12 +15,16 @@ public class FireballSource : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.7f, 0));
 
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit))
         {
             targetPoint.position = hit.point;
+        }
+        else
+        {
+            targetPoint.position = ray.GetPoint(targetInSkyDistance);
         }
 
         transform.LookAt(targetPoint.position);
