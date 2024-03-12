e29832d8b152b8b8c237533e569a57ec8dff66b0
FatChapa <maksimka.sharov2008@gmail.com>
2024-03-12 21:44:15 +0300

    Add ViewportPointToRay (script)

--- Assets/Scripts/FireballSource.cs
+++ Assets/Scripts/FireballSource.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class FireballSource : MonoBehaviour
 {
     public Transform targetPoint;
+    public Camera cameraLink;
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         transform.LookAt(targetPoint.position);
     }
 }
